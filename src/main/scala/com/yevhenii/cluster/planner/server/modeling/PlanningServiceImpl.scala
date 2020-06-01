3e7d1a8653c413de94ab10f09ebdcbb691945ecd
ZheniaTrochun <zheniatrochun@gmail.com>
2020-06-01 17:53:23 +0300

    deduplicated code

--- src/main/scala/com/yevhenii/cluster/planner/server/modeling/PlanningServiceImpl.scala
+++ src/main/scala/com/yevhenii/cluster/planner/server/modeling/PlanningServiceImpl.scala
@@ -3,6 +3,7 @@ package com.yevhenii.cluster.planner.server.modeling
 import cats.Show
 import cats.effect.IO
 import com.typesafe.scalalogging.LazyLogging
+import com.yevhenii.cluster.planner.server.dto.Graphs
 import com.yevhenii.cluster.planner.server.dto.Task.TaskId
 import com.yevhenii.cluster.planner.server.graphs.TaskService
 import com.yevhenii.cluster.planner.server.model.{Node, NonOrientedGraph, NonOrientedGraphEntry, OrientedGraph, OrientedGraphEntry}
@@ -10,15 +11,14 @@ import com.yevhenii.cluster.planner.server.model.{Node, NonOrientedGraph, NonOri
 
 class PlanningServiceImpl(taskService: TaskService) extends PlanningService with LazyLogging {
 
+  type QueueCreator = OrientedGraph => List[Node]
+  type Planner = (NonOrientedGraph, OrientedGraph, QueueCreator) => GhantDiagram
+
   override def buildGhantDiagramByConnectivity(id: TaskId, queueCreator: OrientedGraph => List[Node]): IO[Option[GhantDiagram]] = {
     IO.apply(logger.info("Creating Ghant diagram based on nodes connectivity"))
       .flatMap(_ => taskService.getGraphs(id))
-      .map(_.map { graphs =>
-        val taskGraph = OrientedGraph(OrientedGraphEntry(graphs.taskGraph))
-        val systemGraph = NonOrientedGraph(NonOrientedGraphEntry(graphs.systemGraph))
-
-        val result = ExecutionPlanner.planExecutionByConnectivity(systemGraph, taskGraph, queueCreator)
-
+      .map(_.map(graphs => planning(graphs, queueCreator, ExecutionPlanner.planExecutionByConnectivity)))
+      .map(_.map { result =>
         logger.info(Show[GhantDiagram].show(result))
         result
       })
@@ -27,14 +27,17 @@ class PlanningServiceImpl(taskService: TaskService) extends PlanningService with
   override def buildGhantDiagramByClosestNeighbor(id: TaskId, queueCreator: OrientedGraph => List[Node]): IO[Option[GhantDiagram]] = {
     IO.apply(logger.info("Creating Ghant diagram based on closest neighbor"))
       .flatMap(_ => taskService.getGraphs(id))
-      .map(_.map { graphs =>
-        val taskGraph = OrientedGraph(OrientedGraphEntry(graphs.taskGraph))
-        val systemGraph = NonOrientedGraph(NonOrientedGraphEntry(graphs.systemGraph))
-
-        val result = ExecutionPlanner.planExecutionByClosestNeighbor(systemGraph, taskGraph, queueCreator)
-
+      .map(_.map(graphs => planning(graphs, queueCreator, ExecutionPlanner.planExecutionByClosestNeighbor)))
+      .map(_.map { result =>
         logger.info(Show[GhantDiagram].show(result))
         result
       })
+  }
+
+  private def planning(graphs: Graphs, queueCreator: QueueCreator, planner: Planner): GhantDiagram = {
+    val taskGraph = OrientedGraph(OrientedGraphEntry(graphs.taskGraph))
+    val systemGraph = NonOrientedGraph(NonOrientedGraphEntry(graphs.systemGraph))
+
+    planner(systemGraph, taskGraph, queueCreator)
   }
 }
